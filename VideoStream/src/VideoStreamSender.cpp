57879dcd9c56cd0e749649732c3581972342be16
Minh Quan Tran <32492951+mqtran4793@users.noreply.github.com>
2019-12-03 01:53:08 -0800

    Add files via upload

--- VideoStream/src/VideoStreamSender.cpp
+++ VideoStream/src/VideoStreamSender.cpp
@@ -1,4 +1,7 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <IoTDataThing.hpp>
 #include <JSonThingAPI.hpp>
@@ -24,24 +27,28 @@ int main(){
     return -1;
   }
      
-  while(1){
+  // while(1){
  
     Mat frame;
+    Mat frame_gray;
+    
     // Capture frame-by-frame
     cap >> frame;
-  
+    cvtColor(frame, frame_gray, COLOR_RGB2GRAY);
     // If the frame is empty, break immediately
-    if (frame.empty())
-      break;
+    // if (frame.empty())
+      // break;
  
     // Display the resulting frame
-    imshow( "Frame", frame );
+    // imshow( "Frame", frame );
+    frame_gray = frame_gray.reshape(1, 1);
+    cout << frame_gray.row(0).total() << endl;
  
     // Press  ESC on keyboard to exit
-    char c=(char)waitKey(25);
-    if(c==27)
-      break;
-  }
+    // char c=(char)waitKey(25);
+    // if(c==27)
+      // break;
+  // }
   
   // When everything done, release the video capture object
   cap.release();
