4443218b7093111cfe31d3c0520c8331484416a9
Minh Quan Tran <32492951+mqtran4793@users.noreply.github.com>
2019-12-03 01:08:01 -0800

    Add files via upload

---
+++ VideoStream/build/CMakeFiles/VideoStreamSender.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/minhquantran/ADLINK/EdgeSDK/1.2.0/examples/cpp/VideoStream/src/VideoStreamSender.cpp" "/home/minhquantran/ADLINK/EdgeSDK/1.2.0/examples/cpp/VideoStream/build/CMakeFiles/VideoStreamSender.dir/src/VideoStreamSender.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/intel/openvino_2019.3.376/opencv/include"
+  "/home/minhquantran/ADLINK/EdgeSDK/1.2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
