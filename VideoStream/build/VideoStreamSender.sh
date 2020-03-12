f24803468e05101f3427e21d4ee6c19b137414cb
MinhQuan Tran <mqtran4793@gmail.com>
2020-03-12 13:55:14 -0700

    Clean up

--- VideoStream/build/VideoStreamSender.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export ADLINK_DATARIVER_URI=file://$EDGE_SDK_HOME/etc/config/default_datariver_config_v1.2.xml
-
-THING_PROPERTIES_URI=file://./config/VideoStreamSenderProperties.json
-RUNNING_TIME=${1:-60}
-
-./VideoStreamSender $THING_PROPERTIES_URI $RUNNING_TIME
